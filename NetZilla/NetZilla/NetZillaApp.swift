6f8bbec2e3eea9fbad8f48539a68a487a506d50d
“Davit <2840850M@student.gla.ac.uk>
2025-06-25 15:06:38 +0100

    Initial commit

---
+++ NetZilla/NetZilla/NetZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetZillaApp.swift
+//  NetZilla
+//
+//  Created by Davit Muradyan on 25.06.25.
+//
+
+import SwiftUI
+
+@main
+struct NetZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
